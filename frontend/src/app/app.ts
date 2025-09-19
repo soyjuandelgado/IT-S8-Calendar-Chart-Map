534cd2d85785ef67a7b77f6b6eb04a08e25f82f9
Juan Delgado <soy.juan.delgado@gmail.com>
2025-09-19 10:57:40 +0200

    feat: added menu and page components

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
-import { MapboxMap } from './mapbox-map/mapbox-map';
+import { Menu } from './menu/menu';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule, MapboxMap],
+  imports: [RouterOutlet, ButtonModule, Menu],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ frontend/src/app/menu/menu.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
+
+@Component({
+  selector: 'app-menu',
+  imports: [MenubarModule],
+  templateUrl: './menu.html',
+  styleUrl: './menu.css'
+})
+export class Menu implements OnInit{
+  items: MenuItem[] | undefined;
+
+    ngOnInit() {
+        this.items = [
+            {
+                label: 'Home',
+                icon: 'pi pi-home',
+                routerLink: '/home'
+            },
+            {
+                label: 'Calendar',
+                icon: 'pi pi-calendar',
+                routerLink: '/calendar'
+            },
+            {
+                label: 'Charts',
+                icon: 'pi pi-chart-bar',
+                routerLink: '/chart'
+            },
+            {
+                label: 'Map',
+                icon: 'pi pi-map',
+                routerLink: '/map'
+            },
+        ]
+    }      
+}
