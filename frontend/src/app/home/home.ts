040bd89a13f5ce4421bc547958f762f9b79e93b9
Juan Delgado <soy.juan.delgado@gmail.com>
2025-09-19 11:30:22 +0200

    feat: added home page

--- frontend/src/app/home/home.ts
+++ frontend/src/app/home/home.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CardModule, ButtonModule, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
