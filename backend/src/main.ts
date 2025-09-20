bda6f5040a1e77ff9c67cf58b5c540c1881a43a1
Juan Delgado <soy.juan.delgado@gmail.com>
2025-09-20 13:55:03 +0200

    added API documentation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  // Configurar títulos de documnentación
+  const options = new DocumentBuilder()
+    .setTitle('Users REST API')
+    .setDescription('API REST of Users')
+    .setVersion('1.0')
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT', in: 'header' },
+      'access-token',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+
+  // La ruta en que se sirve la documentación
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
--- backend/src/users/user.entity.ts
+++ backend/src/users/user.entity.ts
@@ -1,25 +1,33 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
+  @ApiProperty({ example: 99 })
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({ example: 'José' })
   @Column()
   first: string;
 
+  @ApiProperty({ example: 'Pérez' })
   @Column()
   last: string;
 
+  @ApiProperty({ example: 'email@email.com' })
   @Column()
   email: string;
 
+  @ApiProperty({ example: '987654321' })
   @Column()
   phone: string;
 
+  @ApiProperty({ example: 'Mataró, Barcelona, Spain' })
   @Column()
   location: string;
 
+  @ApiProperty({ example: 'leer' })
   @Column()
   hobby: string;
 }
